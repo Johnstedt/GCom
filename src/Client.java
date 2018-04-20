c13a9f0206251a988006efe7631e24280b3e90fa
John-John Markstedt <c14jmt@cs.umu.se>
2018-04-20 10:02:38 +0200

    Added ant build script

---
+++ src/Client.java
@@ -0,0 +1,12 @@
+public class Client
+{
+    public static void main (String[] args)
+     {
+         System.out.println("\nBorn as a Client, cradled\n" +
+                 "in a manager, He came forth from heaven to live on Scratchy \n" +
+                 "as a mortal Streaming Service and to establish the connection \n" +
+                 "of UDP. His glorious recursion reshaped the thinking of the \n" +
+                 "world. He streamed for us, and He died for us.\n");
+    }
+
+}
