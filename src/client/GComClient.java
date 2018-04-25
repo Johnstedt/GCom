2a134c4a86f8a7767865f8c5a87fc781a95aece0
Jonathan Westin <jwestin@cs.umu.se>
2018-04-25 18:32:41 +0200

    Added some gui

--- src/client/GComClient.java
+++
@@ -1,9 +0,0 @@
-package client;
-
-import javax.swing.*;
-
-public class GComClient {
-	private JTextField writeToChatTextField;
-	private JList list1;
-	private JList list2;
-}
---
+++ src/client/Test.java
@@ -0,0 +1,48 @@
+package client;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Test extends Application {
+	static String username = "";
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			Scanner reader = new Scanner(System.in);
+			System.out.println("What is your username");
+			username = reader.nextLine();
+			reader.close();
+		}
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			URL url = new File("src/client/client.fxml").toURL();
+			Parent root = FXMLLoader.load(url);
+			Scene scene = new Scene(root, 600, 400);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Hello You");
+			primaryStage.setMinWidth(600);
+			primaryStage.setMinHeight(400);
+			primaryStage.setWidth(600);
+			primaryStage.setHeight(400);
+			primaryStage.setMaximized(false);
+			primaryStage.setFullScreen(false);
+			primaryStage.setResizable(false);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
