76c1422232176511e5aa2364803ed80cecdbf231
Jonathan Westin <jwestin@cs.umu.se>
2018-04-25 10:19:32 +0200

    Fixed tab indention.

--- src/communication/Receiver.java
+++ src/communication/Receiver.java
@@ -4,14 +4,14 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class Receiver {
-  public static void main(String args[]) {
-    try {
-      RemoteObject impl = new RemoteObjectImpl();
-      Registry registry = LocateRegistry.getRegistry();
+	public static void main(String args[]) {
+		try {
+			RemoteObject impl = new RemoteObjectImpl();
+			Registry registry = LocateRegistry.getRegistry();
 
-      registry.rebind("MessageService", impl);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
+			registry.rebind("MessageService", impl);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/communication/Sender.java
+++ src/communication/Sender.java
@@ -4,15 +4,15 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class Sender {
-  public static void main(String args[]) {
-    try {
-      Registry registry = LocateRegistry.getRegistry("localhost");
+	public static void main(String args[]) {
+		try {
+			Registry registry = LocateRegistry.getRegistry("localhost");
 
-      RemoteObject stub = (RemoteObject)registry.lookup("MessageService");
+			RemoteObject stub = (RemoteObject)registry.lookup("MessageService");
 
-      System.out.println("Result: "+stub.printMessage("bingo"));
-    } catch(Exception e) {
-      e.printStackTrace();
-    }
-  }
+			System.out.println("Result: "+stub.printMessage("bingo"));
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
