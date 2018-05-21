48f094d1ef869f7b8a4592ebd49c4d91eaa6f46e
Jonathan Westin <jwestin@cs.umu.se>
2018-05-21 14:06:30 +0200

    Fixed new group dialog

--- src/client/TwoFieldDialog.java
+++ src/client/TwoFieldDialog.java
@@ -1,5 +1,7 @@
 package client;
 
+import group_management.CommunicationType;
+import group_management.MessageOrderingType;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
@@ -7,7 +9,7 @@ import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.util.Pair;
 
-import java.util.Optional;
+import java.util.*;
 
 public class TwoFieldDialog {
 	String val1;
@@ -72,5 +74,74 @@ public class TwoFieldDialog {
 			this.val2 = input.getValue();
 		});
 		return result.isPresent();
+	}
+
+	public HashMap<String, String> newGroup() {
+		Set<String> messageOrdering = new HashSet<>();
+		for (MessageOrderingType mot : MessageOrderingType.values()) {
+			messageOrdering.add(mot.toString());
+		}
+
+		Set<String> communicationType = new HashSet<>();
+		for (CommunicationType ct: CommunicationType.values()) {
+			communicationType.add(ct.toString());
+		}
+
+		TextField field1 = new TextField();
+		field1.setPromptText(val1);
+
+
+		Dialog<HashMap<String, String>> dialog = new Dialog<>();
+		dialog.setTitle("Create new group");
+		dialog.setHeaderText("Lets make an awesome group");
+
+// Set the button types.
+		ButtonType mergeButtonType = new ButtonType("Create group", ButtonBar.ButtonData.OK_DONE);
+		dialog.getDialogPane().getButtonTypes().addAll(mergeButtonType, ButtonType.CANCEL);
+
+		GridPane grid = new GridPane();
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(20, 150, 10, 10));
+
+		ComboBox<String> messageOrderingBox = new ComboBox<>();
+		messageOrdering.forEach((mo) -> {
+			messageOrderingBox.getItems().add(mo);
+		});
+		messageOrderingBox.setValue(messageOrdering.iterator().next());
+
+		ComboBox<String> communicationTypeBox = new ComboBox<>();
+		communicationType.stream().forEach((ct) -> {
+			communicationTypeBox.getItems().add(ct);
+		});
+		communicationTypeBox.setValue(communicationType.iterator().next());
+
+		int i1 = 0;
+		grid.add(new Label("Name:"), 0, i1);
+		grid.add(field1, 1, i1++);
+		grid.add(new Label("Communication Type:"), 0, i1);
+		grid.add(messageOrderingBox, 1, i1++);
+		grid.add(new Label("Ordering Type:"), 0, i1);
+		grid.add(communicationTypeBox, 1, i1++);
+
+		dialog.getDialogPane().setContent(grid);
+		Platform.runLater(() -> field1.requestFocus());
+
+// Convert the result to the desired data structure
+		dialog.setResultConverter(dialogButton -> {
+			if (dialogButton == mergeButtonType) {
+				HashMap<String, String> result = new HashMap<>();
+				result.put("Name", field1.getText());
+				result.put("Message Ordering", messageOrderingBox.getValue());
+				result.put("Ordering Type", communicationTypeBox.getValue());
+				return result;
+			}
+			return null;
+		});
+
+		Optional<HashMap<String, String>> result = dialog.showAndWait();
+
+		return result.orElse(null);
+
 	}
 }
